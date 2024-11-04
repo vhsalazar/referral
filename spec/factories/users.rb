cdaadfe22ed33e5c08038f106cdc582d430176b4
Victor Salazar <victor@ninjadevs.com>
2024-11-04 16:39:50 +1100

    update users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     password_digest { BCrypt::Password.create(Faker::Internet.password(min_length: 8)) }
     referral_code { Faker::Alphanumeric.alphanumeric(number: 10) }
     referred_by { nil } # Allows for users without a referral; can be set to another User if needed
+    status { User.statuses[:bronze] }
 
     # Nested factory for creating a referred user
     factory :user_with_referrer do
