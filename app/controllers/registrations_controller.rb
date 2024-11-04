d52db70d50183925fef08233838ce76b83273e4c
Victor Salazar <victor@ninjadevs.com>
2024-11-04 16:35:20 +1100

    Update registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,11 +6,14 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
+    signup_service = Users::SignupUserService.new(user_params)
+
+    if signup_service.call
+      @user = signup_service.user
       start_new_session_for @user
       redirect_to root_path, notice: "Successfully signed up!"
     else
+      @user = signup_service.user
       flash.now[:alert] = "There was a problem with your registration."
       render :new, status: :unprocessable_entity
     end
@@ -18,6 +21,6 @@ class RegistrationsController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email_address, :password, :password_confirmation)
+    params.require(:user).permit(:email_address, :password, :password_confirmation, :referer_code)
   end
 end
