17fef97d7913c24ed63fcd8daef397d625ac279f
Victor Salazar <victor@ninjadevs.com>
2024-11-04 23:30:22 +1100

    update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,13 @@ class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
+
+  def show
+    @referrals = current_user.referrals
+  end
+
   def new
+    redirect_to root_path if current_user
   end
 
   def create
