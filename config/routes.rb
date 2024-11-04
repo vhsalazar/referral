180aa1f7a3129c8781c7a6d64a8fb89f68b4a6c1
Victor Salazar <victor@ninjadevs.com>
2024-11-04 23:30:31 +1100

    Add profile show

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get "home/index"
   root to: "home#index"
 
+  get "profile" => "sessions#show"
   resource :session
   resource :registration, only: %i[new create]
+
   resources :passwords, param: :token
 
   get "up" => "rails/health#show", as: :rails_health_check
